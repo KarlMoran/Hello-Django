24fa70d5b982f5944547503acbd7c9c1c3c25400
Karl Moran <karlmoran06@gmail.com>
2022-06-13 13:35:45 +0000

    Add tests to test_models

--- todo/test_views.py
+++ todo/test_views.py
@@ -3,9 +3,7 @@ from .models import Item
 
 
 class TestDjango(TestCase):
-    
-    def test_this_thing_work(self):
-        self.assertEqual(1, 1)
+
 
     def test_get_todo_list(self):
         response = self.client.get('/')
@@ -15,13 +13,13 @@ class TestDjango(TestCase):
     def test_get_add_item_page(self):
         response = self.client.get('/add')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'todo/add_list.html')
+        self.assertTemplateUsed(response, 'todo/add_item.html')
     
     def test_get_edit_item_page(self):
-        item = Item.objects.create(name= 'Test Todo Item')
+        item = Item.objects.create(name='Test Todo Item')
         response = self.client.get(f'/edit/{item.id}')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'todo/edit_list.html')
+        self.assertTemplateUsed(response, 'todo/edit_item.html')
     
     def test_can_add_item(self):
         response = self.client.post('/add', {'name': 'Test Added Item'})
@@ -35,7 +33,7 @@ class TestDjango(TestCase):
         self.assertEqual(len(existing_items), 0)
     
     def test_can_toggle_item(self):
-        item = Item.objects.create(name= 'Test Todo Item', done=True)
+        item = Item.objects.create(name='Test Todo Item', done=True)
         response = self.client.get(f'/toggle/{item.id}')
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
